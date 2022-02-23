14561c0a9b81b62a17985763c1953788c10d75ac
Mateusz <mateusz.krzeszowiec@1450.pl>
2022-02-23 10:52:51 +0100

    Section 6 - setup new project

--- Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation, ViewChild, ElementRef, ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -9,9 +9,14 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 export class ServerElementComponent implements OnInit {
   // dodaję dekorator @input by móc zmieniać informację w propercie z zewnętrznego komponentu
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) contentParagraph: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log('Tex tcontent' + this.header.nativeElement.textContent);
   }
 
 }
